66db4edbc00df1438fb55dac91c94e397fc52b30
Justin Stowe <justinstowe12@gmail.com>
2023-03-13 21:54:22 -0600

    removed username from user model

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { SingleMovie } from "./pages/SingleMovie/SingleMovie";
 import Search from "./components/Search/Search";
 import Header from "./components/Header/Header";
 import Aside from "./components/Aside/Aside";
-import { getUser } from "./utilities/users-service";
+import { getUser } from "./utilities/user-services";
 
 import AuthPage from "./pages/AuthPage/AuthPage";
 
@@ -15,34 +15,34 @@ function App() {
   const [user, setUser] = useState(getUser());
   return (
     <div className="App">
-      {/* {user ? ( */}
-      <>
-        <Header />
-        <Aside />
-        <Search />
-        <AuthPage />
+      {user ? (
+        <>
+          <Header />
+          <Aside />
+          <Search />
+          <AuthPage />
 
-        <Routes>
-          <Route
-            path="/moviestowatch"
-            element={<MoviesToWatch user={user} setUser={setUser} />}
-          />
-          <Route
-            path="/watchedmovies"
-            element={<WatchedMovies user={user} setUser={setUser} />}
-          />
-          <Route
-            path="/movie"
-            element={<SingleMovie user={user} setUser={setUser} />}
-          />
+          <Routes>
+            <Route
+              path="/moviestowatch"
+              element={<MoviesToWatch user={user} setUser={setUser} />}
+            />
+            <Route
+              path="/watchedmovies"
+              element={<WatchedMovies user={user} setUser={setUser} />}
+            />
+            <Route
+              path="/movie"
+              element={<SingleMovie user={user} setUser={setUser} />}
+            />
 
-          {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-      </>
-      {/* ) : (
+            {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      ) : (
         <AuthPage setUser={setUser} />
-      )} */}
+      )}
     </div>
   );
 }
