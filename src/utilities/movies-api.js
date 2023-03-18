a531500d43cfeda08bf7bbe8e5703ef6f9f94476
Justin Stowe <justinstowe12@gmail.com>
2023-03-18 13:35:54 -0600

    changes made, code is working ATM

--- src/utilities/movies-api.js
+++ src/utilities/movies-api.js
@@ -21,6 +21,6 @@ export function addMovie(movieData) {
 
 export function updateUser(id, userMovieData) {
   // movieData.completed = true;
-  console.log("Updated mvie api", userMovieData);
-  return sendRequest(`/api/user/${id}`, "PUT", userMovieData);
+  console.log("Updated movie api", userMovieData);
+  return sendRequest(`BASE_URL/${id}`, "PUT", userMovieData);
 }
