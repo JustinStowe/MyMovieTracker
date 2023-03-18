461a5dcb527e3b5703300a23a1a148c91e8b2a4e
amcculley222 <andrewpmcculley22@gmail.com>
2023-03-17 19:56:09 -0700

    comments-api utility

---
+++ src/utilities/comments-api.js
@@ -0,0 +1,12 @@
+import sendRequest from "./send-request.js";
+
+const BASE_URL = `api/movie/${imdbID}`;
+
+export function deleteCommentById(imdbID) {
+  return sendRequest(`${BASE_URL}`);
+}
+
+export function addComment(comment) {
+  console.log("Add Comment", comment);
+  return sendRequest(BASE_URL, "POST", comment);
+}
