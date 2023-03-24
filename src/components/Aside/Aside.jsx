ec5a786315fa5eac1a3882ff278d036f34d66dca
bazilevsd <114525681+bazilevsd@users.noreply.github.com>
2023-03-23 21:50:11 -0700

    update to users display

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -7,8 +7,11 @@ import { useEffect } from "react";
 export default function Aside({ user, setUser }) {
   const { friends, getAllFriends, users, addFriend, getAllUsers } =
     useController();
+  const userId = user._id;
+
   useEffect(() => {
     if (users.length < 1) {
+      getAllUsers();
       getAllFriends();
     }
   }, []);
@@ -17,22 +20,24 @@ export default function Aside({ user, setUser }) {
       {/* <p>{user.name}</p> */}
       <img className={styles.pic} src={user.picture} alt={user.name} />
       <LogOut user={user} setUser={setUser} />
-      {users.map((singleUser) => {
-        return (
-          <div className={styles.div3} key={singleUser._id}>
-            <h3>{singleUser.name}</h3>
-            <button
-              onClick={() => {
-                addFriend(singleUser._id);
-                getAllUsers();
-                // updateMovie(movie._id, movie);
-              }}
-            >
-              Add User to be your friend
-            </button>
-          </div>
-        );
-      })}
+      {users
+        .filter((i) => i._id !== userId)
+        .map((singleUser) => {
+          return (
+            <div className={styles.div3} key={singleUser._id}>
+              <h3>{singleUser.name}</h3>
+              <button
+                onClick={() => {
+                  addFriend(singleUser._id);
+                  getAllUsers();
+                  // updateMovie(movie._id, movie);
+                }}
+              >
+                Add User to be your friend
+              </button>
+            </div>
+          );
+        })}
       {friends.map((friend) => {
         return (
           <div className={styles.div3} key={friend._id}>
